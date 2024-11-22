56e245f3ceb0dd2092e1b3151a11cf86c15cfe51
mmichael255 <cby371@gmail.com>
2024-11-23 08:40:23 +0900

    init '

---
+++ topay-gateway/src/main/java/com/topay/topay_gateway/TopayGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.topay.topay_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopayGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopayGatewayApplication.class, args);
+	}
+
+}
